edd2659dc724dabcd927487f47230c3987c6d347
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-02-08 16:22:15 -0800

    completed a working, albeit messy version of the mockup

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,28 @@
 // Write your JavaScript here...
+const btnBurger = document.getElementById("btn-burger");
+const nav = document.getElementById("main-nav");
+const header = document.getElementsByTagName("header")[0];
+
+// remove default behaviour of burger button focus state
+btnBurger.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+// handle click event on burger button
+btnBurger.addEventListener('click', clickMenu);
+
+function clickMenu(){
+	header.classList.toggle('show'); // show/hide dropdown nav
+	nav.classList.add('activated'); // add transition on nav height
+}
+
+// remove classes when window is resized to reflow width
+window.addEventListener("resize", removeTransition);
+
+function removeTransition(e){
+	if(window.innerWidth > 1000){
+		header.classList.remove('show'); // hide dropdown nav
+        nav.classList.remove('activated'); // remove transition on nav height
+	}
+}
+
